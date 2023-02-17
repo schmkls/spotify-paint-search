a2d00b20b01a1512ea04fb01be9e80387e271b6e
schmkls <mickels.martin@gmail.com>
2023-02-17 19:20:24 +0100

    simple get auth code

--- sp-paint-search/src/index.js
+++ sp-paint-search/src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import queryString from 'query-string';
+
+const authorize = () => {
+    const scope = 'user-read-private';
+    const client_id = '24dcca9ea4c5412098172221557f9e84'
+    const state = '123'
+
+    let auth_url = 'https://accounts.spotify.com/authorize?' + queryString.stringify({
+        response_type: 'code',
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: 'http://localhost:3000/',
+        state: state
+    })
+
+    window.location.href = auth_url
+}
+
+const App = () => {
+    return (
+        <div>    
+            <button onClick={() => authorize()}>Paint search</button>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <App/>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
