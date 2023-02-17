8625948cdf208bdff22b95927929aea740c3698d
schmkls <mickels.martin@gmail.com>
2023-02-18 00:32:37 +0100

    get access token

--- sp-paint-search/src/UnAuthorized.js
+++ sp-paint-search/src/UnAuthorized.js
@@ -1,10 +1,13 @@
 import queryString from 'query-string';
+import { Buffer } from 'buffer';
+
+const client_id = '24dcca9ea4c5412098172221557f9e84'
+const client_secret = 'e2bee5c5d34e4090beb5cf330a7d0284'
+const state = '123'
+const redirect_uri = 'http://localhost:3000/callback/'
 
 const authorize = () => {
     const scope = 'user-read-private';
-    const client_id = '24dcca9ea4c5412098172221557f9e84'
-    const state = '123'
-    const redirect_uri = window.location.href + '/callback/'
     console.log('redirect_uri: ' + redirect_uri);
 
     let auth_url = 'https://accounts.spotify.com/authorize?' + queryString.stringify({
@@ -18,11 +21,31 @@ const authorize = () => {
     window.location.href = auth_url;
 }
 
-const UnAuthorized = () => {
+const finishAuth = (code) => {
+    const query_string = 'https://accounts.spotify.com/api/token?' + queryString.stringify({
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: redirect_uri,
+        client_id: client_id,
+        client_secret: client_secret
+    })
 
+    fetch(query_string, {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')), 
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+    })
+    .then(response => response.json())
+    .then(response => console.log(JSON.stringify(response)))
+}
+
+const UnAuthorized = () => {
     const parameters = new URLSearchParams(window.location.search)
     if (parameters.has('code')) {
-        localStorage.setItem('access_token', '123')
+        finishAuth(parameters.get('code'))
+        //window.history.back()
     }
     
     return (
