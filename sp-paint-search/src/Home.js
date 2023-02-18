cec370970150ac107944dcce635c237b7ecd4f40
schmkls <mickels.martin@gmail.com>
2023-02-18 18:01:25 +0100

    spotify api call works

--- sp-paint-search/src/Home.js
+++ sp-paint-search/src/Home.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from 'react'
+import { Buffer } from 'buffer';
 
 const Home = () => {
 
     const [name, setName] = useState('')
-    const now = Date.now()
 
     useEffect(() => {
         fetch('https://api.spotify.com/v1/me', {
             method: 'GET',
             headers: {
-                'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')), 
-                'Content-Type': 'application/x-www-form-urlencoded',
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('access_token')
             }
-        })
-        .then(response => response.json())
+        }).
+        then(response => response.json())
         .then(json => {
             setName(json.display_name)
         })
@@ -23,9 +24,7 @@ const Home = () => {
     return (
         <div>
             <h1>Home</h1>
-            <h3>{localStorage.getItem('access_token')}</h3>
             <h2>{name}</h2>
-            <h2>{now}</h2>
         </div>
     )
 }
--- sp-paint-search/src/UnAuthorized.js
+++ sp-paint-search/src/UnAuthorized.js
@@ -2,15 +2,13 @@ import queryString from 'query-string';
 import { Buffer } from 'buffer';
 
 const scope = 'user-read-private';
-const client_id = '24dcca9ea4c5412098172221557f9e84'
-const client_secret = 'e2bee5c5d34e4090beb5cf330a7d0284'
 const state = '123'
 const redirect_uri = 'http://localhost:3000/callback/'
 
 const authorize = () => {
     let auth_url = 'https://accounts.spotify.com/authorize?' + queryString.stringify({
         response_type: 'code',
-        client_id: client_id,
+        client_id: process.env.REACT_APP_SP_CLIENT_ID,
         scope: scope,
         redirect_uri: 'http://localhost:3000/callback/',
         state: state
@@ -24,14 +22,14 @@ const getAccessTokenThenGoBack = (code) => {
         grant_type: 'authorization_code',
         code: code,
         redirect_uri: redirect_uri,
-        client_id: client_id,
-        client_secret: client_secret
+        client_id: process.env.REACT_APP_SP_CLIENT_ID,
+        client_secret: process.env.REACT_APP_SP_CLIENT_SECRET
     })
 
     fetch(query_string, {
         method: 'POST',
         headers: {
-            'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')), 
+            'Authorization': 'Basic ' + (new Buffer(process.env.REACT_APP_SP_CLIENT_ID + ':' + process.env.REACT_APP_SP_CLIENT_SECRET).toString('base64')), 
             'Content-Type': 'application/x-www-form-urlencoded',
         }
     })
